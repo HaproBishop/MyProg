df7285c1c40208cf7c7ac2908ed278a4705e44a4
Hapro <Hapro@DESKTOP-MLAUF43>
2021-09-19 16:57:48 +0300

    Добавьте файлы проекта.

---
+++ MyProg/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WorkNotepad;
+
+namespace MyProg
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();    
+            
+        }
+        
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog
+            {
+                Title = "Открытие файла",
+                Filter = "Файл(.txt) | *.txt",
+                DefaultExt = ".txt",
+            };
+            if (open.ShowDialog() == true)
+            {
+                OwnText.Text = Class_WorkNotepad.OpenFile(open.FileName);
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog
+            {
+                Title = "Сохранение файла",
+                Filter = "Файл(.txt) | *.txt",
+                DefaultExt = ".txt",
+            };
+            if (save.ShowDialog() == true)
+            {
+                Class_WorkNotepad.SaveFile(save.FileName, OwnText.Text);
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Vid_Click(object sender, RoutedEventArgs e)
+        {
+            Vid windowvid = new Vid();
+            if (windowvid.ShowDialog() == true) 
+            {
+                OwnText.Text = "AHAHA";
+            }
+        }
+
+        private void AbProg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MyProg/Vid.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WorkNotepad;
+
+namespace MyProg
+{
+    /// <summary>
+    /// Логика взаимодействия для Vid.xaml
+    /// </summary>
+    public partial class Vid : Window
+    {
+        public Vid()
+        {
+            InitializeComponent();
+        }
+
+        private void LImpact_Selected(object sender, RoutedEventArgs e)
+        {
+          
+        }
+        public static void VidClick()
+        {
+            
+        }
+
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            Class_WorkNotepad.SaveSettings(FontChangeSize_TextBox.Text, StyleSelect.Text);
+            
+        }
+
+        private void StyleSegoeUI_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TimesNewRoman_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StyleArial_ComboBox_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
