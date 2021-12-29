91f4add5b12cc0dd0a8b943077d636aa30cbf652
Hapro <acfowo45@gmail.com>
2021-12-29 21:50:10 +0300

    Усиленная переработка интерфейса в настройках P.S. Планируется добавить образец

--- MyProg/Settings.xaml.cs
+++ MyProg/Settings.xaml.cs
@@ -24,6 +24,7 @@ namespace MyProg
         public SettingsWindow()
         {
             InitializeComponent();
+            ListSize.ItemsSource = new List<int>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
         }
         private void SaveSettings_Click(object sender, RoutedEventArgs e)
         {
