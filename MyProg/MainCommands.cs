518c73c36fccf1100095732faba122ad8c353903
Hapro <acfowo45@gmail.com>
2022-01-05 15:47:24 +0300

    Исправлены наименования для переменных InputGestureCollection

--- MyProg/MainCommands.cs
+++ MyProg/MainCommands.cs
@@ -11,26 +11,26 @@ namespace MyProg
     {
         static MainCommands()
         {
-            InputGestureCollection input1 = new InputGestureCollection()
+            InputGestureCollection inputScalePlus = new InputGestureCollection()
             {
                 new KeyGesture(Key.OemPlus, ModifierKeys.Control, "Ctrl+Плюс(+)") 
             };
-            CommandScalePlus = new RoutedUICommand("CommandScalePlus", "MainCommands", typeof(MainCommands), input1);
-            InputGestureCollection input2 = new InputGestureCollection()
+            CommandScalePlus = new RoutedUICommand("CommandScalePlus", "MainCommands", typeof(MainCommands), inputScalePlus);
+            InputGestureCollection inputScaleMinus = new InputGestureCollection()
             {
                 new KeyGesture(Key.OemMinus, ModifierKeys.Control, "Ctrl+Минус(-)")
             };
-            CommandScaleMinus = new RoutedUICommand("CommandScaleMinus", "MainCommands", typeof(MainCommands), input2);
-            InputGestureCollection input3 = new InputGestureCollection()
+            CommandScaleMinus = new RoutedUICommand("CommandScaleMinus", "MainCommands", typeof(MainCommands), inputScaleMinus);
+            InputGestureCollection inputDefaultScale = new InputGestureCollection()
             {
                 new KeyGesture(Key.D9|Key.D0, ModifierKeys.Control, "Ctrl+()")
             };
-            CommandDefaultScale = new RoutedUICommand("CommandDefaultScale", "MainCommands", typeof(MainCommands), input3);
-            InputGestureCollection input4 = new InputGestureCollection()
+            CommandDefaultScale = new RoutedUICommand("CommandDefaultScale", "MainCommands", typeof(MainCommands), inputDefaultScale);
+            InputGestureCollection inputCurrentDateAndTime = new InputGestureCollection()
             {
                 new KeyGesture(Key.F5, ModifierKeys.None, "F5")
             };
-            CommandCurrentDateAndTime = new RoutedUICommand("CommandCurrentDateAndTime", "MainCommands", typeof(MainCommands), input4);
+            CommandCurrentDateAndTime = new RoutedUICommand("CommandCurrentDateAndTime", "MainCommands", typeof(MainCommands), inputCurrentDateAndTime);
         }
         public static RoutedCommand CommandScalePlus { get; private set; }
         public static RoutedCommand CommandScaleMinus { get; private set; }
